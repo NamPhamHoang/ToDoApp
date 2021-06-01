b654c5d7f39305784ed7cc31f7d81e216fdc4046
NamPhamHoang <phamhoangnam55@gmail.com>
2021-06-01 09:46:35 +0700

    [UPDATE] middleware, response common, update return new, handling 404 error

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import bodyParser from "body-parser";
-import createTasks from "./routes/createTasks";
-import deleteTask from "./routes/deleteTask";
+import createTasks from "./routes/createTasks.route";
+import deleteTask from "./routes/deleteTask.route";
+import NotFound from "./routes/notFound.route";
 // tslint:disable-next-line:ordered-imports
 import express from "express";
 import multer from "multer";
-import listTasks from "./routes/listTasks";
-// tslint:disable-next-line:ordered-imports
 import path from "path";
-import updateTask from "./routes/updateTask";
-import {connect} from "./utils/database";
-
+import { response } from "./utils/response.util";
+// tslint:disable-next-line:ordered-imports
+import validateMiddleWare from "./modules/validate.module";
+import listTasks from "./routes/listTasks.route";
+import updateTask from "./routes/updateTask.route";
+import {connect} from "./utils/database.util";
 const PORT = 3000;
 const app = express();
 
@@ -32,28 +34,25 @@ const tempUpload = multer({
 
 app.use(bodyParser.json())
 
+app.use(NotFound)
 app.use("/uploads", express.static('./src/uploads'));
 
-app.post("/createTask", createTasks)
+app.post("/createTask", validateMiddleWare, createTasks)
 
 app.post("/uploadFile", tempUpload.single("file"), async (req, res) => {
     try {
         if(req.file !== undefined) {
-            res.status(200).json({
-                isError: false,
-                payload: "Upload Success"
-            })
+            response(res).success({
+                message: "Upload Successful"
+            });
         } else {
-            res.status(200).json({
-                isError: false,
-                payload: "Please choose file"
-            })
+            response(res).success({
+                message: "Please Choose File"
+            });
         }
-     
     } catch(err) {
-        res.status(500).json({
-            isError: true,
-            payload: err.toString()
+        response(res).error(500,{
+            message: err.toString
         })
     }
 })
@@ -62,7 +61,7 @@ app.get("/listTask", listTasks)
 
 app.get("/delete/:task_id", deleteTask)
 
-app.post("/update/", updateTask)
+app.post("/update/", validateMiddleWare, updateTask)
 
 app.listen(PORT, () => {
     console.log(`> The application is running on PORT ${PORT}`)
